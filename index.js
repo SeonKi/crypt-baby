65d992018004cf6f5e535b64aa26c77ae2bf3e82
Seonki <seongkim824@gmail.com>
2017-06-22 00:30:18 -0400

    changes to exchanges

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const express = require('express')
 const app = express()
 const request = require('request')
@@ -7,12 +8,11 @@ const http = require('http')
 let io = require('socket.io-client')
 const socket = io('wss://streamer.cryptocompare.com')
 
-let subscription = ['2~Kraken~ETH~BTC','2~Kraken~XMR~BTC','2~Kraken~SC~BTC','2~Kraken~ANS~BTC','2~Kraken~ANS~BTC','2~Kraken~STRAT~BTC','2~Kraken~BTC~USD','2~Kraken~XEM~BTC',
-'2~Yunbi~ETH~CNY','2~Yunbi~SC~CNY','2~Yunbi~ANS~CNY','2~Yunbi~GNT~CNY','2~Yunbi~REP~CNY',
-'2~Coinone~ETH~KRW','2~Coinone~BTC~KRW','2~Coinone~XRP~KRW','2~Coinone~ETC~KRW',
-'2~OKCoin~LTC~CNY','2~OKCoin~ETH~CNY','2~OKCoin~BTC~CNY',
-'2~BitTrex~ETH~BTC','2~BitTrex~XMR~BTC','2~BitTrex~SC~BTC','2~BitTrex~ANS~BTC','2~BitTrex~ANS~BTC','2~BitTrex~STRAT~BTC','2~BitTrex~BTC~USD','2~BitTrex~XEM~BTC',
-'2~Poloniex~ETH~BTC','2~Poloniex~XMR~BTC','2~Poloniex~SC~BTC','2~Poloniex~ANS~BTC','2~BitTrex~STRAT~BTC','2~BitTrex~XEM~BTC'
+let subscription = ['2~BitFinex~BTC~USD','2~Coinbase~BTC~USD','2~Coinone~BTC~KRW','2~Kraken~BTC~USD','2~OKCoin~BTC~CNY','2~BitTrex~BTC~USD','2~BTCC~BTC~CNY','2~Korbit~BTC~KRW',
+'2~Kraken~ETH~BTC','2~Poloniex~ETH~BTC','2~BitTrex~ETH~BTC','2~Yunbi~ETH~CNY','2~Coinbase~ETH~USD','2~OKCoin~ETH~CNY','2~Korbit~ETH~KRW',
+'2~BitTrex~XMR~BTC','2~Poloniex~XMR~BTC','2~Bitfinex~XMR~BTC','2~Kraken~XMR~BTC',
+'2~Yunbi~ANS~BTC','2~BitTrex~ANS~BTC',
+'2~BitTrex~XRP~BTC','2~Coinone~XRP~KRW','2~Poloniex~XRP~BTC'
 ]
 
 socket.emit('SubAdd', {subs:subscription} );
@@ -20,7 +20,6 @@ socket.emit('SubAdd', {subs:subscription} );
 socket.on('m', function (data) {
   fs.appendFile('./dump.txt', `${data}|`, function (err) {
   if (err) throw err;
-  console.log('Saved!');
 });
   });
 
